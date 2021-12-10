28cd7808167bc1976b747bb1b7377a97b6923701
Gurtej736l <gurtej154@gmail.com>
2021-12-11 09:41:14 +1030

    connect utils-auth file to the userRoutes.js

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const { User } = require("../../models");
+const withAuth = require("../../utils/auth");
 
 // CREATE new user
-router.post("/", async (req, res) => {
+router.post("/", withAuth, async (req, res) => {
   try {
     const dbUserData = await User.create({
       username: req.body.username,
