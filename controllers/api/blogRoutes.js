66ddc0cb8b9c16c6e570dcaae8cd3d4bec512abd
Gurtej736l <gurtej154@gmail.com>
2021-12-11 12:25:46 +1030

    add comment creation and likes to blog post in blogRoutes.js file

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,13 +1,36 @@
 const router = require("express").router();
-const { Blog, Comment, User } = require();
+const { Blog, Comment, Like, User } = require();
 const withAuth = require("../../utils/auth");
 
+// Creates a blog (post)
 router.post("/", withAuth, (req, res) => {
   const body = req.body;
   console.log(req.session.userID);
   Blog.create({ ...body, userID: req.session.userID })
     .then((newBlog) => {
       res.json(newBlog);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+// User can comment on a blog post
+router.post("/", withAuth, (req, res) => {
+  Comment.create({ ...req.body, userID: req.session.userID })
+    .then((newComment) => {
+      res.json(newComment);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+// User can like blog post
+router.post("/", withAuth, (req, res) => {
+  Like.add({ ...req.body, userID: req.session.userID })
+    .then((newLike) => {
+      res.json(newLike);
     })
     .catch((err) => {
       res.status(500).json(err);
