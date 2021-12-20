45034089e5153ba5861575d45d0a726ef8cf9060
Hadis Parsa <parsa_pary1988@yahoo.com>
2021-12-20 17:33:49 +1000

    post button

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -32,7 +32,17 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
+router.get('/blog', async (req, res) => {
+  try {
+    res.render('post', {
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err);
+  }
 
+})
 //hadis 
 router.get('/blog/:id', async (req, res) => {
   try {
